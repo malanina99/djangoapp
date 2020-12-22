aee481292ccbc229697b59980747dbc4ef6b3956
malanina99 <olya.malanina1999@gmail.com>
2020-12-23 02:19:35 +0700

    deploy1

--- btre/settings.py
+++ btre/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -155,5 +154,3 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_PASSWORD='jkz0810vfkfz332123jkz'
-
-django_heroku.settings(locals())
